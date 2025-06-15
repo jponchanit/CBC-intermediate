b9ecea2a0e92c42a774d964cca53d447f8843e90
PONCHANIT\ADMIN26 <text>
2025-06-15 10:47:14 +0700

    update

--- 03-ObjectOriented/practice/inheritance_practice.py
+++ 03-ObjectOriented/practice/inheritance_practice.py
@@ -4,8 +4,12 @@ class Person():
         self.name = name 
         
 class Teacher(Person):
-    def teach(self) :
-        print(f"")
+    def __init__(self, name, name_t):
+        super().def __init__(self, name):
+            self.name = name
+            self.name_t = name_t
+    def teacher(self):
+        
         
 # Example:
 # p = Person("Alice")
